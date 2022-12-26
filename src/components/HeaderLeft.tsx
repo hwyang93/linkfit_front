d73e00f20e8d91037e5db2fe94c82be7c94b6d45
mrbong <sm841004@gmail.com>
2022-12-26 21:46:03 +0900

    header left 추가

--- src/components/HeaderLeft.tsx
+++ src/components/HeaderLeft.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import {Pressable, Text} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import {useNavigation} from '@react-navigation/native';
 
-const HeaderLeft = () => {
+type HeaderLeftProps = {
+  canGoBack: boolean;
+  tintColor: string;
+};
+
+const HeaderLeft = ({canGoBack, tintColor}: HeaderLeftProps) => {
+  const navigation = useNavigation();
+  if (!canGoBack) {
+    return null;
+  }
   return (
-    <Pressable>
+    <Pressable onPress={navigation.goBack} hitSlop={10}>
       <Text>
-        <Icon name="angle-left" size={24} color="#000" />
+        <Icon name="angle-left" size={24} color={tintColor} />
       </Text>
     </Pressable>
   );
