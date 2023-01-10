376d263a719037c5a2ae47b974b3cabc0c89c764
mrbong <sm841004@gmail.com>
2023-01-10 12:11:29 +0900

    alias

--- src/navigations/AuthStack.tsx
+++ src/navigations/AuthStack.tsx
@@ -1,10 +1,10 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {WHITE} from '../styles/colors';
-import HeaderLeft from '../components/HeaderLeft';
-import SignIn from '../screen/SignIn';
-import Login from '../screen/Login';
-import SignUp from '../screen/SignUp';
-import PasswordReset from '../screen/PasswordReset';
+import {WHITE} from '@styles/colors';
+import HeaderLeft from '@components/HeaderLeft';
+import SignIn from '@screen/SignIn';
+import Login from '@screen/Login';
+import SignUp from '@screen/SignUp';
+import PasswordReset from '@screen/PasswordReset';
 import * as React from 'react';
 import {RootStackParamList} from '../../AppInner';
 
