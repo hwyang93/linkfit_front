79820a3180f6890ea0c0f4987cce9f491560184b
mrbong <sm841004@gmail.com>
2023-01-26 08:40:24 +0900

    사업자 회원가입

--- src/screen/Registration/CompanySignUpFormScreen.tsx
+++ src/screen/Registration/CompanySignUpFormScreen.tsx
@@ -3,6 +3,7 @@ import {
   Pressable,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from 'react-native';
 import common from '@styles/common';
@@ -13,6 +14,9 @@ import {INPUT, WHITE} from '@styles/colors';
 import LinearGradient from 'react-native-linear-gradient';
 import SelectDropdown from 'react-native-select-dropdown';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+import TabButton from '@components/TabButton';
+import moment from 'moment';
 
 function SignUpFormScreen() {
   const [businessNumber, setBusinessNumber] = useState<number>();
@@ -20,14 +24,33 @@ function SignUpFormScreen() {
   const [ownerName, setOwnerName] = useState<string>('');
   const [postNumber, setPostNumber] = useState<number>();
   const [companyAddress, setCompanyAddress] = useState<string>('');
-
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [birthday, setBirthday] = useState('');
+  const [gender, setGender] = useState('');
+  const [agency, setAgency] = useState('');
   const [category, setCategory] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
   const [focus, setFocus] = useState(false);
 
   const categoryData = ['필라테스', '요가'];
+  const genderData = [{value: '남자'}, {value: '여자'}];
+  const agencyData = ['SKT', 'KT', 'LG U+', '알뜰폰'];
 
   const [loading, setLoading] = useState<boolean>(false);
   const canGoNext = false;
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+  const handleConfirm = (date: any) => {
+    setBirthday(moment(date).format('YYYY.MM.DD'));
+    hideDatePicker();
+  };
   // todo : 사업자 등록번호 형식에 맞게 입력하면 disable 해제,
   // todo : 사업자 등록번호가 등록된 번호 일 경우
 
@@ -103,7 +126,7 @@ function SignUpFormScreen() {
         </View>
 
         {/* 업종 */}
-        <View style={{flex: 1}}>
+        <View style={common.mb16}>
           <SelectDropdown
             data={categoryData}
             onSelect={selectedItem => {
@@ -135,7 +158,109 @@ function SignUpFormScreen() {
             onFocus={() => setFocus(true)}
           />
         </View>
-        {/*  */}
+        {/* 생년월일  */}
+        <View style={common.mb16}>
+          <TouchableOpacity onPress={showDatePicker}>
+            <Input
+              pointerEvents={'none'}
+              label={'생년월일'}
+              value={birthday}
+              placeholder={'생년월일을 선택 하세요.'}
+            />
+            <DateTimePickerModal
+              isVisible={isDatePickerVisible}
+              mode="date"
+              onConfirm={handleConfirm}
+              onCancel={hideDatePicker}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={[common.mb16]}>
+          <TabButton
+            genderData={genderData}
+            onSelect={(value: any) => setGender(value)}
+            value={gender}
+          />
+        </View>
+        {/* 통신사 */}
+        <View
+          style={[
+            common.mb16,
+            {
+              flex: 1,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            },
+          ]}>
+          <View style={{flex: 1, marginRight: 8}}>
+            <SelectDropdown
+              data={agencyData}
+              onSelect={selectedItem => {
+                setAgency(selectedItem);
+                console.log('selected : ', selectedItem);
+              }}
+              buttonTextAfterSelection={selectedItem => {
+                return selectedItem;
+              }}
+              rowTextForSelection={item => {
+                return item;
+              }}
+              defaultButtonText={'통신사'}
+              buttonStyle={focus ? styles.selectBoxFocus : styles.selectBox}
+              buttonTextStyle={
+                focus ? styles.selectTextFocus : styles.selectText
+              }
+              renderDropdownIcon={isOpened => {
+                return (
+                  <FontAwesome
+                    name={isOpened ? 'chevron-up' : 'chevron-down'}
+                    color={'#acacac'}
+                    size={16}
+                  />
+                );
+              }}
+              dropdownIconPosition={'right'}
+              dropdownStyle={styles.dropBox}
+              rowStyle={styles.dropItem}
+              rowTextStyle={styles.dropText}
+              onFocus={() => setFocus(true)}
+            />
+          </View>
+          <View style={{flex: 2}}>
+            <Input
+              label={'휴대폰 번호'}
+              onChangeText={(text: any) => setPhoneNumber(text)}
+              value={phoneNumber}
+              placeholder={'01012345678'}
+              keyboardType={KeyboardTypes.PHONE}
+            />
+          </View>
+        </View>
+        {/* 비밀번호 */}
+        <View>
+          <View style={common.mb16}>
+            <Input
+              label={'비밀번호'}
+              onChangeText={(text: string) => setPassword(text.trim())}
+              value={password}
+              placeholder={'비밀번호를 입력하세요.'}
+              keyboardType={KeyboardTypes.DEFAULT}
+              secureTextEntry
+            />
+          </View>
+        </View>
+
+        <View style={common.mb16}>
+          <Input
+            label={'비밀번호 확인'}
+            onChangeText={(text: string) => setPasswordConfirm(text.trim())}
+            value={passwordConfirm}
+            placeholder={'비밀번호를 다시한번 입력하세요.'}
+            keyboardType={KeyboardTypes.DEFAULT}
+            secureTextEntry
+          />
+        </View>
+
         <View style={common.mt20}>
           <Pressable onPress={() => console.log(category)}>
             <LinearGradient
@@ -162,7 +287,6 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     color: WHITE,
   },
-
   selectBox: {
     width: '100%',
     height: 56,
