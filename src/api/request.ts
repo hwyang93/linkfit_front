a44d8679e73379364d3b1e613a8976a681a01ea9
mrbong <sm841004@gmail.com>
2023-01-26 11:33:44 +0900

    임시 커밋

--- src/api/request.ts
+++ src/api/request.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const service = axios.create({
   // 배포전이므로 현재 서버 구동 아이피 주소로 변경
-  baseURL: 'http://172.30.1.16:3000/api/v1',
+  baseURL: 'http://172.30.1.68:3000/api/v1',
   timeout: 600000,
 });
 service.interceptors.response.use(
