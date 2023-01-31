8def9b68ff4a7b093e53c7a224ddd577277f2485
mrbong <sm841004@gmail.com>
2023-01-31 15:25:32 +0900

    충돌 해결을 위한 임시 커밋

---
+++ src/api/request.ts
@@ -0,0 +1,38 @@
+const axios = require('axios').default;
+import EncryptedStorage from 'react-native-encrypted-storage';
+
+const service = axios.create({
+  baseURL: 'http://127.0.0.1:3000/api/v1',
+  timeout: 600000,
+});
+export const getHeaders = async () => {
+  const headers = {
+    Authorization: '',
+  };
+  const token = await EncryptedStorage.getItem('accessToken');
+  if (token) {
+    headers.Authorization = `Bearer ${token}`;
+  }
+
+  return headers;
+};
+
+service.interceptors.request.use(
+  async (config: {headers: any}) => {
+    config.headers = {...config.headers, ...(await getHeaders())};
+    console.log(config.headers);
+    return config;
+  },
+  (error: any) => {
+    return Promise.reject(error);
+  },
+);
+service.interceptors.response.use(
+  function (response: {data: any}) {
+    return response.data;
+  },
+  function (error: any) {
+    return Promise.reject(error);
+  },
+);
+export default service;
