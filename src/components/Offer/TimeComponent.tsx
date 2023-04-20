3752c345981dd07cc8915c32a68c2e9229ce6cd9
yanghw <yanghw0911@gmail.com>
2023-04-20 15:46:05 +0900

    - 공고 작성 기능 개발

--- src/components/Offer/TimeComponent.tsx
+++ src/components/Offer/TimeComponent.tsx
@@ -2,20 +2,34 @@ import {View} from 'react-native';
 import common from '@styles/common';
 import BirthdayPicker from '@components/BirthdayPicker';
 import SelectBox from '@components/SelectBox';
-import {useState} from 'react';
+import {useCallback, useState} from 'react';
+type timeProps = {
+  onSelectDay: Function;
+  onSelectTime: Function;
+};
+const TIME2 = ['오전', '오후', '전일', '시간 협의'];
+function TimeComponent({onSelectDay, onSelectTime}: timeProps) {
+  const [day, setDay] = useState('');
+  const [, setTime] = useState('');
 
-function TimeComponent() {
-  const [date, setDate] = useState('');
-  const [time, setTime] = useState('');
-  const TIME2 = ['오전', '오후', '전일', '시간 협의'];
+  const onSelectDatePicker = (value: string) => {
+    onSelectDay(value);
+    setDay(value);
+  };
+
+  // const onClickTime = (value: string) => {
+  //   onSelectTime(value);
+  //   setTime(value);
+  // };
+
   return (
     <View>
       <View style={common.mb16}>
         <BirthdayPicker
           label={'날짜'}
-          onSelect={(value: any) => setDate(value)}
+          onSelect={(value: any) => onSelectDatePicker(value)}
           placeholder={'날짜를 선택하세요.'}
-          value={date}
+          value={day}
           textAlign={'right'}
           icon={'day'}
         />
@@ -24,7 +38,7 @@ function TimeComponent() {
         <SelectBox
           label={'시간'}
           data={TIME2}
-          onSelect={(value: any) => setTime(value)}
+          onSelect={(value: any) => onSelectTime(value)}
           defaultButtonText={'선택한 요일의 시간을 선택하세요.'}
           textAlign={'right'}
           icon={'time'}
