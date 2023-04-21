0a8b3e08c9799e69acbabe522cf8f13a486d4f5a
yanghw <yanghw0911@gmail.com>
2023-04-21 16:55:34 +0900

    - 이력서 등록 작업중

--- src/api/resume.ts
+++ src/api/resume.ts
@@ -1,5 +1,9 @@
 import request from '@api/request';
 
+export function createResume(data: object) {
+  return request.post('/resume', data);
+}
+
 export function fetchResumes() {
   return request.get('/resume');
 }
