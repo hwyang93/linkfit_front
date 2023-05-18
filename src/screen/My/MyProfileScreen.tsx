2a8275e3ce12bee1dae3e37de9670928636a3f88
nonon <nononcrust@gmail.com>
2023-05-19 00:27:18 +0900

    chore: 프로필 페이지 탭 컴포넌트 머티리얼 탭으로 변경

--- src/screen/My/MyProfileScreen.tsx
+++ src/screen/My/MyProfileScreen.tsx
@@ -6,9 +6,9 @@ import {
   Text,
   Pressable,
   Alert,
+  FlatList,
 } from 'react-native';
-import {Tabs, MaterialTabBar} from 'react-native-collapsible-tab-view';
-import {BLUE, GRAY, WHITE} from '@styles/colors';
+import {BLUE, GRAY} from '@styles/colors';
 import common from '@styles/common';
 import {iconPath} from '@/utils/iconPath';
 import {
@@ -17,276 +17,298 @@ import {
   useNavigation,
 } from '@react-navigation/native';
 import {LoggedInParamList} from '../../../AppInner';
-import {useCallback, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {fetchMemberInfo} from '@api/member';
 import toast from '@hooks/toast';
+import {
+  MaterialTopTabNavigationOptions,
+  createMaterialTopTabNavigator,
+} from '@react-navigation/material-top-tabs';
+import {isAxiosError} from 'axios';
+import {FetchMemberInfoResponse} from '@/types/api/member';
+import {ImageSourcePropType} from 'react-native';
 
-// const HEADER_HEIGHT = 250;
+const DUMMY_MY_INTRODUCTION_DATA = [
+  {src: require('@images/instructor_01.png')},
+  {src: require('@images/instructor_02.png')},
+  {src: require('@images/instructor_03.png')},
+  {src: require('@images/instructor_04.png')},
+  {src: require('@images/instructor_05.png')},
+];
+
+const DUMMY_REVIEW_DATA = [
+  {
+    id: 1,
+    nickname: '저팔계',
+    type: '강사',
+    date: '2022.12.12',
+    review:
+      '후기 내용 입니다. 저팔계지만 유연해요. 깜짝 놀랐어요. 오늘 점심은 뭐 먹을까요. 매일매일 고민해요. 왜 때문이죠.',
+  },
+  {
+    id: 2,
+    nickname: '소다늠',
+    type: '강사',
+    date: '2023.1.12',
+    review: '젓가락이지만 유연해요. 깜짝 놀랐어요.',
+  },
+];
+
+const Tab = createMaterialTopTabNavigator();
 
 const width = Dimensions.get('window').width - 32;
 const tabWidth = width / 2;
 const imageSize = (width - 6) / 3;
 
-function Header() {
+interface HeaderProps {
+  profileImageOriginFileUrl?: string;
+  nickname?: string;
+  name?: string;
+  field?: string;
+  career?: string | null;
+  address?: string;
+  followerCount?: number;
+  onPencilIconPress: () => void;
+}
+
+const Header: React.FC<HeaderProps> = ({
+  profileImageOriginFileUrl,
+  nickname,
+  name,
+  field,
+  career,
+  address,
+  followerCount,
+  onPencilIconPress,
+}) => {
+  return (
+    <View style={styles.profileBox}>
+      <View style={[common.mr16, styles.thumbnailBox]}>
+        <Image
+          source={
+            profileImageOriginFileUrl
+              ? {uri: profileImageOriginFileUrl}
+              : iconPath.THUMBNAIL
+          }
+          style={common.thumbnail_l}
+        />
+      </View>
+      <View>
+        <View style={common.rowCenter}>
+          <Text style={[common.text_l, common.fwb, common.mr8]}>
+            {nickname ? nickname : name}
+          </Text>
+          <View style={common.rowCenter}>
+            <Text style={[common.text_s, {color: BLUE.DEFAULT}]}>인증강사</Text>
+            <Image
+              style={{marginLeft: 2, width: 14, height: 14}}
+              source={iconPath.CERTIFICATION}
+            />
+          </View>
+        </View>
+
+        <View style={common.rowCenter}>
+          <Text style={[common.text_m, common.fwb, common.mr8]}>{field}</Text>
+          <Text style={[common.text]}>{career}</Text>
+          <Text style={[common.mh8]}>|</Text>
+          <Text style={[common.text_s, common.fcg]}>{address}</Text>
+        </View>
+
+        <View style={common.rowCenter}>
+          <Pressable onPress={() => Alert.alert('click', 'test')}>
+            <Image
+              source={iconPath.FAVORITE_FILL}
+              style={[common.size24, common.mr8]}
+            />
+          </Pressable>
+          <Text style={[common.text_m, common.fwb, common.mr8]}>
+            {followerCount}
+          </Text>
+        </View>
+      </View>
+
+      <Pressable style={styles.pencil} onPress={onPencilIconPress}>
+        <Image source={iconPath.PENCIL_B} style={[common.size24]} />
+      </Pressable>
+    </View>
+  );
+};
+
+const MyIntroductionTabHeader: React.FC = () => {
+  return (
+    <View style={common.mb8}>
+      <Text style={[common.text_m, common.fwb]}>포트폴리오</Text>
+    </View>
+  );
+};
+
+type ReviewTabItemProps = {
+  nickname: string;
+  type: string;
+  date: string;
+  review: string;
+};
+
+const ReviewTabItem: React.FC<ReviewTabItemProps> = ({
+  nickname,
+  type,
+  date,
+  review,
+}) => {
+  const [textLine, setTextLine] = useState(2);
+
+  const expandText = () => setTextLine(textLine === 2 ? 0 : 2);
+
+  return (
+    <>
+      <View style={[common.row, common.mb8]}>
+        <Text style={[common.text_m, common.fwb, common.fs18]}>{nickname}</Text>
+        <Text
+          style={[common.text, {alignSelf: 'flex-end', marginHorizontal: 4}]}>
+          {type}
+        </Text>
+        <Text style={[common.text, {alignSelf: 'flex-end'}]}>{date}</Text>
+      </View>
+      <Pressable onPress={expandText}>
+        <Text style={common.text_m} numberOfLines={textLine}>
+          {review}
+        </Text>
+      </Pressable>
+    </>
+  );
+};
+
+interface MyIntroductionTabItemProps {
+  src: ImageSourcePropType;
+  onPress?: () => void;
+}
+
+const MyIntroductionTabItem: React.FC<MyIntroductionTabItemProps> = ({
+  src,
+  onPress,
+}) => {
+  return (
+    <Pressable onPress={onPress}>
+      <Image
+        source={src}
+        resizeMode={'cover'}
+        style={{
+          width: imageSize,
+          height: imageSize,
+          margin: 1,
+        }}
+      />
+    </Pressable>
+  );
+};
+
+function MyProfileScreen() {
+  const [memberInfo, setMemberInfo] = useState<FetchMemberInfoResponse>();
+
   const navigation = useNavigation<NavigationProp<LoggedInParamList>>();
-  const [memberInfo, setMemberInfo] = useState<any>({});
   const isFocused = useIsFocused();
+
   useEffect(() => {
     if (isFocused) {
       fetchMemberInfo()
-        .then(({data}: any) => {
+        .then(({data}) => {
           setMemberInfo(data);
         })
-        .catch((e: any) => {
-          toast.error({message: e.message});
+        .catch(error => {
+          if (isAxiosError(error)) {
+            toast.error({message: error.message});
+          }
         });
     }
   }, [isFocused]);
-  return (
-    <View>
-      <View style={styles.profileBox}>
-        <View style={[common.mr16, styles.thumbnailBox]}>
-          <Image
-            source={
-              memberInfo.profileImage
-                ? {uri: memberInfo.profileImage.originFileUrl}
-                : iconPath.THUMBNAIL
-            }
-            style={common.thumbnail_l}
-          />
-        </View>
-        <View>
-          <View style={common.rowCenter}>
-            <Text style={[common.text_l, common.fwb, common.mr8]}>
-              {memberInfo.nickname ? memberInfo.nickname : memberInfo.name}
-            </Text>
-            <View style={common.rowCenter}>
-              <Text style={[common.text_s, {color: BLUE.DEFAULT}]}>
-                인증강사
-              </Text>
-              <Image
-                style={{marginLeft: 2, width: 14, height: 14}}
-                source={iconPath.CERTIFICATION}
-              />
-            </View>
-          </View>
 
-          <View style={common.rowCenter}>
-            <Text style={[common.text_m, common.fwb, common.mr8]}>
-              {memberInfo.field}
-            </Text>
-            <Text style={[common.text]}>{memberInfo.career}</Text>
-            <Text style={[common.mh8]}>|</Text>
-            <Text style={[common.text_s, common.fcg]}>
-              {memberInfo.address}
-            </Text>
-          </View>
-
-          <View style={common.rowCenter}>
-            <Pressable onPress={() => Alert.alert('click', 'test')}>
-              <Image
-                source={iconPath.FAVORITE_FILL}
-                style={[common.size24, common.mr8]}
-              />
-            </Pressable>
-            <Text style={[common.text_m, common.fwb, common.mr8]}>
-              {memberInfo.followerCount}
-            </Text>
-          </View>
-        </View>
-
-        <Pressable
-          style={styles.pencil}
-          onPress={() => navigation.navigate('ProfileEdit', {memberInfo})}>
-          <Image source={iconPath.PENCIL_B} style={[common.size24]} />
-        </Pressable>
-      </View>
-    </View>
-  );
-}
-// 센터 프로필 상단 영역 끝
-
-function MyProfileScreen() {
-  const navigation = useNavigation<NavigationProp<LoggedInParamList>>();
-
-  const tab1Data = [
-    {src: require('@images/instructor_01.png')},
-    {src: require('@images/instructor_02.png')},
-    {src: require('@images/instructor_03.png')},
-    {src: require('@images/instructor_04.png')},
-    {src: require('@images/instructor_05.png')},
-  ];
-  const tab2Data = [
-    {
-      id: 1,
-      nickname: '저팔계',
-      type: '강사',
-      date: '2022.12.12',
-      review:
-        '후기 내용 입니다. 저팔계지만 유연해요. 깜짝 놀랐어요. 오늘 점심은 뭐 먹을까요. 매일매일 고민해요. 왜 때문이죠.',
-    },
-    {
-      id: 2,
-      nickname: '소다늠',
-      type: '강사',
-      date: '2023.1.12',
-      review: '젓가락이지만 유연해요. 깜짝 놀랐어요.',
-    },
-  ];
-
-  const IntroduceTabHeader = () => {
-    return (
-      <View style={common.mv8}>
-        <Text style={[common.text_m, common.fwb]}>포트폴리오</Text>
-      </View>
-    );
-  };
-
-  // 탭 바 영역
-  const tabBar = (props: any) => (
-    <MaterialTabBar
-      {...props}
-      style={styles.tab}
-      inactiveColor={GRAY.DEFAULT}
-      indicatorStyle={styles.indicator}
-      activeColor={BLUE.DEFAULT}
-      itemStyle={{width: tabWidth}}
-      labelStyle={common.text_m}
-      contentContainerStyle={{
-        // flex: 1,
-        width: width,
-      }}
+  const renderMyIntroductionTabItem = ({
+    item,
+  }: {
+    item: MyIntroductionTabItemProps;
+  }) => (
+    <MyIntroductionTabItem
+      src={item.src}
+      onPress={() => navigation.navigate('Gallery')}
     />
   );
 
-  type imageProps = {
-    item: any;
-  };
-
-  const IntroduceTab = useCallback(
-    ({item}: imageProps) => {
-      return (
-        <>
-          <Pressable onPress={() => navigation.navigate('Gallery')}>
-            <Image
-              source={item.src}
-              resizeMode={'cover'}
-              style={{
-                width: imageSize,
-                height: imageSize,
-                margin: 1,
-              }}
-            />
-          </Pressable>
-        </>
-      );
-    },
-    [navigation],
+  const renderReviewTabItem = ({item}: {item: ReviewTabItemProps}) => (
+    <ReviewTabItem
+      nickname={item.nickname}
+      type={item.type}
+      date={item.date}
+      review={item.review}
+    />
   );
 
-  type reviewProps = {
-    item: {
-      id: number;
-      nickname: string;
-      type: string;
-      date: string;
-      review: string;
-    };
+  const MyIntroductionTab: React.FC = () => {
+    return (
+      <FlatList
+        data={DUMMY_MY_INTRODUCTION_DATA}
+        renderItem={renderMyIntroductionTabItem}
+        numColumns={3}
+        ListHeaderComponent={<MyIntroductionTabHeader />}
+        style={{padding: 16, backgroundColor: 'white'}}
+      />
+    );
   };
 
-  const [textLine, setTextLine] = useState(2);
+  const ReviewTab: React.FC = () => {
+    return (
+      <FlatList
+        data={DUMMY_REVIEW_DATA}
+        renderItem={renderReviewTabItem}
+        keyExtractor={item => String(item.id)}
+        style={{padding: 16, backgroundColor: 'white'}}
+        ItemSeparatorComponent={() => (
+          <View style={[common.separator, common.mv16, {width: width}]} />
+        )}
+      />
+    );
+  };
 
-  const ReviewTab = useCallback(
-    ({item}: reviewProps) => {
-      const textExpansion = () => {
-        if (textLine === 2) {
-          setTextLine(0);
-        } else {
-          setTextLine(2);
-        }
-      };
-      return (
-        <View
-          style={{
-            width: width,
-            // padding: 16,
-          }}>
-          <View style={[common.row, common.mb8]}>
-            <Text style={[common.text_m, common.fwb, common.fs18]}>
-              {item.nickname}
-            </Text>
-            <Text
-              style={[
-                common.text,
-                {alignSelf: 'flex-end', marginHorizontal: 4},
-              ]}>
-              {item.type}
-            </Text>
-            <Text style={[common.text, {alignSelf: 'flex-end'}]}>
-              {item.date}
-            </Text>
-          </View>
-          <Pressable onPress={textExpansion}>
-            <Text style={common.text_m} numberOfLines={textLine}>
-              {item.review}
-            </Text>
-          </Pressable>
-        </View>
-      );
+  const materialTopTabNavigationOption: MaterialTopTabNavigationOptions = {
+    tabBarLabelStyle: common.text_m,
+    tabBarInactiveTintColor: GRAY.DEFAULT,
+    tabBarItemStyle: {width: tabWidth},
+    tabBarIndicatorStyle: styles.indicator,
+    tabBarStyle: {
+      marginHorizontal: 16,
     },
-    [textLine],
-  );
+    tabBarLabel: ({focused, children}) => (
+      <Text
+        style={[common.text_m, {color: focused ? BLUE.DEFAULT : GRAY.DEFAULT}]}>
+        {children}
+      </Text>
+    ),
+  };
 
   return (
-    <Tabs.Container
-      renderHeader={Header}
-      headerContainerStyle={{
-        paddingTop: 16,
-        paddingHorizontal: 16,
-        shadowOpacity: 0,
-        elevation: 0,
-      }}
-      // headerHeight={HEADER_HEIGHT}
-      renderTabBar={tabBar}>
-      <Tabs.Tab name="내 소개">
-        <View style={{padding: 16}}>
-          <Tabs.FlatList
-            data={tab1Data}
-            ListHeaderComponent={IntroduceTabHeader}
-            renderItem={IntroduceTab}
-            numColumns={3}
-            keyExtractor={(item: any) => item.id}
-          />
-        </View>
-      </Tabs.Tab>
-      <Tabs.Tab name="받은 후기">
-        <View style={{padding: 16}}>
-          <Tabs.FlatList
-            data={tab2Data}
-            ListHeaderComponent={<View style={{paddingBottom: 16}} />}
-            ItemSeparatorComponent={() => {
-              return (
-                <View style={[common.separator, common.mv16, {width: width}]} />
-              );
-            }}
-            renderItem={ReviewTab}
-            keyExtractor={(item: any) => item.id}
-          />
-        </View>
-      </Tabs.Tab>
-    </Tabs.Container>
+    <>
+      <Header
+        profileImageOriginFileUrl={memberInfo?.profileImage.originFileUrl}
+        nickname={memberInfo?.nickname}
+        name={memberInfo?.name}
+        field={memberInfo?.field}
+        career={memberInfo?.career}
+        address={memberInfo?.address}
+        followerCount={memberInfo?.followerCount}
+        onPencilIconPress={() =>
+          navigation.navigate('ProfileEdit', {memberInfo})
+        }
+      />
+      <Tab.Navigator screenOptions={materialTopTabNavigationOption}>
+        <Tab.Screen name="내 소개" component={MyIntroductionTab} />
+        <Tab.Screen name="받은 후기" component={ReviewTab} />
+      </Tab.Navigator>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  pencil: {position: 'absolute', top: 0, right: 0},
-  tab: {
-    backgroundColor: WHITE,
-    borderBottomWidth: 1,
-    borderColor: GRAY.LIGHT,
-  },
+  pencil: {position: 'absolute', top: 16, right: 16},
   indicator: {
-    width: tabWidth,
     backgroundColor: BLUE.DEFAULT,
   },
   profileBox: {
@@ -294,7 +316,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'flex-start',
     alignItems: 'center',
-    paddingBottom: 16,
+    padding: 16,
   },
   thumbnailBox: {
     alignItems: 'center',
@@ -304,8 +326,6 @@ const styles = StyleSheet.create({
     borderRadius: 40,
     backgroundColor: GRAY.LIGHT,
   },
-  // thumbnail: {width: '50%', height: '50%'},
-  kebabIcon: {position: 'absolute', top: 0, right: 0},
 });
 
 export default MyProfileScreen;
