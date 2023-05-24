167f83c11f1aaac6b176a870b2f05ee2c508e6f5
nonon <nononcrust@gmail.com>
2023-05-24 22:53:05 +0900

    Skeleton 컴포넌트 추가

---
+++ src/components/Common/Skeleton.tsx
@@ -0,0 +1,35 @@
+import THEME from '@/styles/palette';
+import React from 'react';
+import {StyleProp, View, ViewProps, ViewStyle} from 'react-native';
+
+interface SkeletonProps extends ViewProps {
+  variant: 'text' | 'circle' | 'rect';
+  width: number;
+  height: number;
+  style?: StyleProp<ViewStyle>;
+}
+
+const Skeleton: React.FC<SkeletonProps> = ({
+  variant = 'rect',
+  width,
+  height,
+  style,
+  ...props
+}) => {
+  return (
+    <View
+      style={[
+        {
+          backgroundColor: THEME.GREY04,
+          borderRadius: variant === 'circle' ? width / 2 : 4,
+          width: width,
+          height: height,
+        },
+        style,
+      ]}
+      {...props}
+    />
+  );
+};
+
+export default Skeleton;
