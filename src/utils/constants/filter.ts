1e9dfd81a597adf9dd51394b3af808c4c7f440c3
nonon <nononcrust@gmail.com>
2023-06-09 23:05:50 +0900

    filter 상수 추가

---
+++ src/utils/constants/filter.ts
@@ -0,0 +1,23 @@
+const FILTER = {
+  REPLY_OR_NOT: [
+    {label: '답변 대기중', value: ''},
+    {label: '답변 수락', value: ''},
+    {label: '답변 거절', value: ''},
+    {label: '제안 마감', value: ''},
+  ],
+  PERIOD: [
+    {label: '일주일', value: ''},
+    {label: '1개월', value: ''},
+    {label: '2개월', value: ''},
+    {label: '3개월 이상', value: ''},
+  ],
+  STATUS: [
+    {label: '지원 완료', value: ''},
+    {label: '지원 취소', value: ''},
+    {label: '열람', value: ''},
+    {label: '합격', value: ''},
+    {label: '불합격', value: ''},
+  ],
+};
+
+export default FILTER;
