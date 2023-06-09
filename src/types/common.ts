5de1d7ba5b345142a1950ba68501ee7481ff2e2f
nonon <nononcrust@gmail.com>
2023-06-09 22:35:51 +0900

    Filter 옵션 관련 타입 추가

--- src/types/common.ts
+++ src/types/common.ts
@@ -16,3 +16,11 @@ export interface AxiosResponseWithPagingInfo<T> extends AxiosResponse<T> {
     perPage: string;
   };
 }
+
+export type PeriodFilter = '일주일' | '1개월' | '2개월' | '3개월 이상';
+export type StatusFilter =
+  | '지원 완료'
+  | '지원 취소'
+  | '열람'
+  | '합격'
+  | '불합격';
