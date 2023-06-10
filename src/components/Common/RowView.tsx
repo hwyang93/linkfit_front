511d58a49cef1440027a52cd5833c94d397e1b89
nonon <nononcrust@gmail.com>
2023-06-10 22:46:34 +0900

    RowView 컴포넌트 추가

---
+++ src/components/Common/RowView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, ViewProps} from 'react-native';
+
+const RowView: React.FC<ViewProps> = ({children, style, ...props}) => {
+  return (
+    <View style={[{flexDirection: 'row'}, style]} {...props}>
+      {children}
+    </View>
+  );
+};
+
+export default RowView;
