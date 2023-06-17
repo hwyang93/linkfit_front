c16c05e7e1c5a92c186780af16983c21a273aa6a
nonon <nononcrust@gmail.com>
2023-06-17 20:04:44 +0900

    센터 정보 스크린 UI 추가

--- src/screen/CenterInfoScreen.tsx
+++ src/screen/CenterInfoScreen.tsx
@@ -1,58 +1,43 @@
-import {
-  CompanyEntity,
-  MemberReputationEntity,
-  RecruitEntity,
-} from '@/types/api/entities';
+import ExpandButton from '@/components/Common/ExpandButton';
+import IconButton from '@/components/Common/IconButton';
+import RowView from '@/components/Common/RowView';
+import SectionHeader from '@/components/Common/SectionHeader';
+import CenterProfile from '@/components/Compound/CenterProfile';
+import RecruitCard from '@/components/Compound/RecruitCard';
+import ReviewListItem from '@/components/Compound/ReviewListItem';
+import SRC from '@/utils/constants/assets';
 import {SCREEN_WIDTH} from '@/utils/constants/common';
+import {iconPath} from '@/utils/iconPath';
 import {fetchCompany} from '@api/company';
-import CenterInfoTop from '@components/CenterInfoTop';
-import EmptySet from '@components/EmptySet';
 import toast from '@hooks/toast';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {BLUE, GRAY, WHITE} from '@styles/colors';
-import common from '@styles/common';
 import {isAxiosError} from 'axios';
-import {useCallback, useEffect, useState} from 'react';
-import {Image, Pressable, StyleSheet, Text, View} from 'react-native';
-import {
-  MaterialTabBar,
-  TabBarProps,
-  Tabs,
-} from 'react-native-collapsible-tab-view';
+import {useCallback, useEffect} from 'react';
+import {Image, Pressable, ScrollView, Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import {LoggedInParamList} from '../../AppInner';
 
-const width = SCREEN_WIDTH - 32;
-const tabWidth = width / 2;
-const imageSize = (width - 6) / 3;
-
-const tabBar = (props: TabBarProps) => (
-  <MaterialTabBar
-    {...props}
-    style={styles.tab}
-    inactiveColor={GRAY.DEFAULT}
-    indicatorStyle={styles.indicator}
-    activeColor={BLUE.DEFAULT}
-    labelStyle={common.text_m}
-    contentContainerStyle={{
-      flex: 1,
-      width: width,
-    }}
-  />
-);
+const DUMMY_IMAGES = [
+  require('@images/center_01.png'),
+  require('@images/center_02.png'),
+  require('@images/center_03.png'),
+  require('@images/center_04.png'),
+  require('@images/center_05.png'),
+];
 
 type Props = NativeStackScreenProps<LoggedInParamList, 'CenterInfo'>;
 
-const CenterInfoScreen = ({navigation, route}: Props) => {
-  const [centerInfo, setCenterInfo] = useState<CompanyEntity>();
-  const [recruits, setRecruits] = useState<RecruitEntity[]>();
-  const [reputations, setReputations] = useState<MemberReputationEntity[]>();
+const CenterInfoScreen = ({route}: Props) => {
+  // const [centerInfo, setCenterInfo] = useState<CompanyEntity>();
+  // const [recruits, setRecruits] = useState<RecruitEntity[]>();
+  // const [reputations, setReputations] = useState<MemberReputationEntity[]>();
 
   const getCenterInfo = useCallback(() => {
     fetchCompany(route.params.memberSeq)
-      .then(({data}) => {
-        setCenterInfo(data.companyInfo);
-        setRecruits(data.recruits);
-        setReputations(data.reputations);
+      .then(() => {
+        // setCenterInfo(data.companyInfo);
+        // setRecruits(data.recruits);
+        // setReputations(data.reputations);
       })
       .catch(error => {
         if (isAxiosError(error)) {
@@ -65,164 +50,105 @@ const CenterInfoScreen = ({navigation, route}: Props) => {
     getCenterInfo();
   }, [getCenterInfo]);
 
-  const IntroduceTabHeader: React.FC = () => {
-    return (
-      <View style={common.mb8}>
-        <Text style={[common.text_m, common.fwb]}>센터 사진</Text>
-      </View>
-    );
-  };
-
-  const IntroduceTabFooter: React.FC = () => {
-    return (
-      <View style={common.mt16}>
-        <Text style={[common.text_m, common.fwb]}>센터 주소</Text>
-        <Text style={common.text_m}>
-          {`${centerInfo?.address} ${centerInfo?.addressDetail}`}
-        </Text>
-      </View>
-    );
-  };
-
-  const tab1Data = [
-    {src: require('@images/center_01.png')},
-    {src: require('@images/center_02.png')},
-    {src: require('@images/center_03.png')},
-    {src: require('@images/center_04.png')},
-    {src: require('@images/center_05.png')},
-  ];
-
-  type imageProps = {
-    item: any;
-  };
-
-  const IntroduceTab = useCallback(
-    ({item}: imageProps) => {
-      return (
-        <Pressable onPress={() => navigation.navigate('Gallery')}>
-          <Image
-            source={item.src}
-            resizeMode={'cover'}
-            style={{
-              width: imageSize,
-              height: imageSize,
-              margin: 1,
-            }}
-          />
-        </Pressable>
-      );
-    },
-    [navigation],
-  );
-
-  const [textLine, setTextLine] = useState(2);
-
-  const ReviewTab = useCallback(
-    ({item}: any) => {
-      const textExpansion = () => {
-        if (textLine === 2) {
-          setTextLine(0);
-        } else {
-          setTextLine(2);
-        }
-      };
-      return (
-        <>
-          {reputations && reputations.length < 1 ? (
-            <View style={{flex: 1}}>
-              <EmptySet text={'등록된 후기가 없어요.'} />
-            </View>
-          ) : (
-            <View
-              style={{
-                width: width,
-                // padding: 16,
-              }}>
-              <View style={[common.row, common.mb8]}>
-                <Text style={[common.text_m, common.fwb, common.fs18]}>
-                  {item.evaluationMember?.nickname
-                    ? item.evaluationMember?.nickname
-                    : item.evaluationMember?.name}
-                </Text>
-                <Text
-                  style={[
-                    common.text,
-                    {alignSelf: 'flex-end', marginHorizontal: 4},
-                  ]}>
-                  {item.evaluationMember?.field}
-                </Text>
-                <Text style={[common.text, {alignSelf: 'flex-end'}]}>
-                  {item.updatedAt}
-                </Text>
-              </View>
-              <Pressable onPress={textExpansion}>
-                <Text style={common.text_m} numberOfLines={textLine}>
-                  {item.comment}
-                </Text>
-              </Pressable>
-            </View>
-          )}
-        </>
-      );
-    },
-    [textLine, reputations],
-  );
-
   return (
-    <Tabs.Container
-      renderTabBar={tabBar}
-      renderHeader={() =>
-        recruits ? (
-          <CenterInfoTop centerInfo={centerInfo} recruits={recruits} />
-        ) : null
-      }
-      allowHeaderOverscroll
-      headerContainerStyle={{
-        paddingTop: 16,
-        paddingHorizontal: 16,
-        shadowOpacity: 0,
-        elevation: 0,
-      }}>
-      <Tabs.Tab name="센터 소개">
-        <Tabs.FlatList
-          style={{padding: 16}}
-          data={tab1Data}
-          ListHeaderComponent={() => <IntroduceTabHeader />}
-          ListFooterComponent={() => <IntroduceTabFooter />}
-          renderItem={IntroduceTab}
-          numColumns={3}
-          keyExtractor={(item: any) => item.id}
-        />
-      </Tabs.Tab>
-      <Tabs.Tab name="후기 관리">
-        <Tabs.FlatList
-          style={{padding: 16}}
-          data={reputations}
-          ListHeaderComponent={<View style={{paddingBottom: 16}} />}
-          ItemSeparatorComponent={() => {
-            return (
-              <View style={[common.separator, common.mv16, {width: width}]} />
-            );
-          }}
-          renderItem={ReviewTab}
-          keyExtractor={(item: any) => item.seq}
-        />
-      </Tabs.Tab>
-    </Tabs.Container>
+    <SafeAreaView edges={['left', 'right']} style={{flex: 1}}>
+      <ScrollView>
+        <View style={{margin: 16}}>
+          <Image
+            style={{width: '100%', height: 160, borderRadius: 8}}
+            source={SRC.IMAGES.CENTER01}
+          />
+          <View style={{marginTop: 16}}>
+            <CenterProfile
+              name="링크 필라테스"
+              isFavorite={false}
+              favoriteCount="23"
+              field="필라테스"
+              address="서울 송파구"
+            />
+          </View>
+          <SectionHeader title="소개글" style={{marginTop: 16}} />
+          <Text style={{fontSize: 16, marginTop: 8}}>
+            강남구 역삼동에 위치해있는 필라테스 센터입니다.
+          </Text>
+          <RowView style={{marginTop: 24, justifyContent: 'space-between'}}>
+            <SectionHeader title="링크" />
+            <RowView>
+              <IconButton source={iconPath.LINK_URL} />
+              <IconButton
+                source={iconPath.LINK_BLOG}
+                style={{marginLeft: 16}}
+              />
+              <IconButton
+                source={iconPath.LINK_BRUNCH}
+                style={{marginLeft: 16}}
+              />
+            </RowView>
+          </RowView>
+          <SectionHeader title="채용 중" style={{marginTop: 24}} />
+          <RecruitCard
+            style={{marginTop: 8}}
+            title="필라테스 강사님 모십니다."
+            recruitType="파트"
+            date="월,수,금"
+            time="시간협의"
+            bookmarked
+          />
+          <RecruitCard
+            style={{marginTop: 8}}
+            title="필라테스 강사님 모십니다."
+            recruitType="파트"
+            date="월,수,금"
+            time="시간협의"
+            bookmarked
+          />
+          <ExpandButton
+            style={{marginTop: 8}}
+            expanded={false}
+            onPress={() => {}}
+          />
+          <SectionHeader title="센터 사진" style={{marginTop: 24}} />
+          <RowView style={{flexWrap: 'wrap', marginTop: 8}}>
+            {DUMMY_IMAGES.map((item, index) => (
+              <Pressable
+                key={index}
+                style={{
+                  flexDirection: 'row',
+                  width: (SCREEN_WIDTH - 38) / 3,
+                  height: (SCREEN_WIDTH - 38) / 3,
+                  margin: 1,
+                }}>
+                <Image source={item} style={{width: '100%', height: '100%'}} />
+              </Pressable>
+            ))}
+          </RowView>
+          <SectionHeader
+            style={{marginTop: 24}}
+            title="센터 후기"
+            onPress={() => {}}
+          />
+          <ReviewListItem
+            nickname="닉네임"
+            role="강사"
+            timestamp="2022.12.12"
+            content="후기 내용입니다. 후기 내용입니다. 후기 내용입니다. 후기 내용입니다."
+          />
+          <ReviewListItem
+            nickname="닉네임"
+            role="강사"
+            timestamp="2022.12.12"
+            content="후기 내용입니다. 후기 내용입니다. 후기 내용입니다. 후기 내용입니다."
+          />
+          <ReviewListItem
+            nickname="닉네임"
+            role="강사"
+            timestamp="2022.12.12"
+            content="후기 내용입니다. 후기 내용입니다. 후기 내용입니다. 후기 내용입니다."
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  pencil: {position: 'absolute', top: 0, right: 0},
-  tab: {
-    backgroundColor: WHITE,
-    borderBottomWidth: 1,
-    borderColor: GRAY.LIGHT,
-  },
-  indicator: {
-    width: tabWidth,
-    backgroundColor: BLUE.DEFAULT,
-  },
-});
 
 export default CenterInfoScreen;
