7870081811f2ef9cbf8f73292bbd73d80bc21ace
nonon <nononcrust@gmail.com>
2023-08-05 16:03:02 +0900

    패스워드 재설정 페이지 UI 추가

--- src/screen/Registration/PasswordResetScreen.tsx
+++ src/screen/Registration/PasswordResetScreen.tsx
@@ -1,117 +1,150 @@
+import BoxButton from '@/components/Common/BoxButton';
 import CTAButton from '@/components/Common/CTAButton';
-import BirthdayPicker from '@components/BirthdayPicker';
+import RowView from '@/components/Common/RowView';
+import TextField from '@/components/Common/TextField';
+import useInput from '@/hooks/useInput';
 import DismissKeyboardView from '@components/DismissKeyboardView';
-import Input, {KeyboardTypes} from '@components/Input';
-import SelectBox from '@components/SelectBox';
-import TabButton from '@components/TabButton';
+import {KeyboardTypes} from '@components/Input';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import common from '@styles/common';
 import {useState} from 'react';
 import {Text, View} from 'react-native';
 import {LoggedInParamList} from '../../../AppInner';
 
-const GENDER_DATA = [{value: '남자'}, {value: '여자'}];
-const AGENCY_DATA = ['SKT', 'KT', 'LG U+', '알뜰폰'];
-const LOADING = false;
-const CAN_GO_NEXT = true;
-
 type Props = NativeStackScreenProps<LoggedInParamList, 'PasswordReset'>;
 
 const PasswordResetScreen = ({}: Props) => {
-  const [userName, setUserName] = useState('');
-  const [birth, setBirth] = useState('');
-  const [gender, setGender] = useState('');
-  const [agency, setAgency] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [step, setStep] = useState(1);
+  const [verificationCodeSent, setVerificationCodeSent] = useState(false);
+
+  const emailInput = useInput();
+  const verificationCodeInput = useInput();
+  const passwordInput = useInput();
+  const passwordConfirmInput = useInput();
+
+  // TODO: 검증 추가
+  const isEmailInputValid = emailInput.value.length > 0;
+  const isVerificationCodeInputValid = verificationCodeInput.value.length > 0;
+
+  const isPasswordInputValid = passwordInput.value.length > 0;
+  const isPasswordConfirmInputValid =
+    passwordConfirmInput.value === passwordInput.value;
+
+  const onSendButtonPress = () => {
+    try {
+      setVerificationCodeSent(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onResendButtonPress = () => {};
+
+  const onVerifyButtonPress = () => {
+    setStep(2);
+  };
+
+  const onResetButtonPress = () => {};
 
   return (
     <DismissKeyboardView>
       <View style={common.container}>
-        <View>
-          <Text style={[common.text_m, common.tal]}>
-            비밀번호 재설정을 위해{'\n'} 가입한 정보를 입력해 주세요.
-          </Text>
-        </View>
-        <View style={common.mt40}>
-          <View style={common.mb16}>
-            <Input
-              label={'이름'}
-              onChangeText={(text: string) => setUserName(text.trim())}
-              value={userName}
-              placeholder={'이름을 입력 하세요.'}
-              keyboardType={KeyboardTypes.DEFAULT}
-            />
-          </View>
-          <View style={common.mb16}>
-            <BirthdayPicker
-              label={'생년월일'}
-              onSelect={(value: any) => setBirth(value)}
-              placeholder={'생년월일을 선택하세요.'}
-              value={birth}
-            />
-          </View>
-          <View style={[common.mb16]}>
-            <TabButton
-              genderData={GENDER_DATA}
-              onSelect={(value: any) => setGender(value)}
-              value={gender}
-            />
-          </View>
-
-          <View style={[common.mb16, common.row]}>
-            <View style={{flex: 1, marginRight: 8}}>
-              <SelectBox
-                data={AGENCY_DATA}
-                onSelect={(value: any) => setAgency(value)}
-                defaultButtonText={'통신사'}
+        {step === 1 && (
+          <>
+            <View>
+              <Text style={{fontSize: 24, fontWeight: '700'}}>STEP 1</Text>
+              <Text style={[common.text_m, common.tal, common.mt16]}>
+                인증번호 재설정을 위해서{'\n'}가입한 이메일을 입력해 주세요.
+              </Text>
+            </View>
+            <View style={common.mt40}>
+              <View style={common.mb16}>
+                <RowView style={{alignItems: 'center'}}>
+                  <TextField
+                    style={[common.mr8, {flex: 1}]}
+                    label="이메일"
+                    onChangeText={emailInput.onChange}
+                    value={emailInput.value}
+                    placeholder="name@email.com"
+                    keyboardType={KeyboardTypes.DEFAULT}
+                    error={emailInput.value.length !== 0 && !isEmailInputValid}
+                    errorMessage="이메일 형식에 맞게 입력해 주세요."
+                    editable={!verificationCodeSent}
+                  />
+                  {!verificationCodeSent ? (
+                    <BoxButton
+                      label="전송"
+                      onPress={onSendButtonPress}
+                      disabled={!isEmailInputValid}
+                    />
+                  ) : (
+                    <BoxButton label="재전송" onPress={onResendButtonPress} />
+                  )}
+                </RowView>
+              </View>
+              <View>
+                {verificationCodeSent && (
+                  <TextField
+                    label="인증번호"
+                    placeholder="인증번호 6자리를 입력하세요."
+                    value={verificationCodeInput.value}
+                    onChangeText={verificationCodeInput.onChange}
+                    error={
+                      verificationCodeInput.value.length !== 0 &&
+                      !isVerificationCodeInputValid
+                    }
+                    errorMessage="인증번호 자릿수를 확인하세요."
+                  />
+                )}
+              </View>
+            </View>
+            <View style={common.mt40}>
+              <CTAButton
+                label="확인"
+                onPress={onVerifyButtonPress}
+                disabled={!isEmailInputValid || !isVerificationCodeInputValid}
               />
             </View>
-            <View style={{flex: 2}}>
-              <Input
-                label={'휴대폰 번호'}
-                onChangeText={(text: any) => setPhoneNumber(text)}
-                value={phoneNumber}
-                placeholder={'01012345678'}
-                keyboardType={KeyboardTypes.PHONE}
-              />
+          </>
+        )}
+        {step === 2 && (
+          <>
+            <View>
+              <Text style={{fontSize: 24, fontWeight: '700'}}>STEP 2</Text>
+              <Text style={[common.text_m, common.tal, common.mt16]}>
+                새로운 비밀번호를 입력해 주세요.
+              </Text>
             </View>
-          </View>
-
-          <View>
-            <View style={common.mb16}>
-              <Input
-                label={'비밀번호'}
-                onChangeText={(text: string) => setPassword(text.trim())}
-                value={password}
-                placeholder={'비밀번호를 입력하세요.'}
+            <View style={common.mt40}>
+              <TextField
+                style={common.mb16}
+                label="비밀번호"
+                placeholder="비밀번호를 입력하세요."
                 keyboardType={KeyboardTypes.DEFAULT}
                 secureTextEntry
+                value={passwordInput.value}
+                onChangeText={passwordInput.onChange}
+                errorMessage="비밀번호 형식을 확인하세요."
+              />
+              <TextField
+                label="비밀번호 확인"
+                placeholder="비밀번호를 다시 한번 입력하세요."
+                keyboardType={KeyboardTypes.DEFAULT}
+                secureTextEntry
+                value={passwordConfirmInput.value}
+                onChangeText={passwordConfirmInput.onChange}
+                errorMessage="비밀번호를 확인하세요."
               />
             </View>
-          </View>
-
-          <View style={common.mb16}>
-            <Input
-              label={'비밀번호 확인'}
-              onChangeText={(text: string) => setPasswordConfirm(text.trim())}
-              value={passwordConfirm}
-              placeholder={'비밀번호를 다시한번 입력하세요.'}
-              keyboardType={KeyboardTypes.DEFAULT}
-              secureTextEntry
-            />
-          </View>
-
-          <View style={common.mt20}>
-            <CTAButton
-              label="본인인증"
-              loading={LOADING}
-              disabled={!CAN_GO_NEXT}
-              onPress={() => console.log(agency)}
-            />
-          </View>
-        </View>
+            <View style={common.mt40}>
+              <CTAButton
+                label="비밀번호 재설정"
+                onPress={onResetButtonPress}
+                disabled={!isPasswordInputValid || !isPasswordConfirmInputValid}
+              />
+            </View>
+          </>
+        )}
       </View>
     </DismissKeyboardView>
   );
