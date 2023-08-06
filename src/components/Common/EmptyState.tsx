180124de29b811db005dbd005687cc2be9042db5
nonon <nononcrust@gmail.com>
2023-08-06 17:47:52 +0900

    EmptyState 컴포넌트 수정

--- src/components/Common/EmptyState.tsx
+++ src/components/Common/EmptyState.tsx
@@ -1,15 +1,26 @@
+import THEME from '@/styles/theme';
 import React from 'react';
 import {Text, View} from 'react-native';
 
-const EmptyState: React.FC = () => {
+interface EmptyStateProps {
+  message: string;
+  height?: number;
+}
+
+// TODO: 플레이스홀더 이미지 추가
+
+const EmptyState: React.FC<EmptyStateProps> = ({message, height}) => {
   return (
     <View
       style={{
         justifyContent: 'center',
         alignContent: 'center',
         flex: 1,
+        height: height ? height : 180,
       }}>
-      <Text style={{textAlign: 'center'}}>아무것도 없습니다</Text>
+      <Text style={{fontSize: 16, color: THEME.GREY02, textAlign: 'center'}}>
+        {message}
+      </Text>
     </View>
   );
 };
