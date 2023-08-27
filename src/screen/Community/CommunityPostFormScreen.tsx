667cc8cdfa0154bd62e81693c50aed8d9de7aaa5
nonon <nononcrust@gmail.com>
2023-08-27 12:26:21 +0900

    CommunityPostForm 스크린 폼 밸리데이션 추가

--- src/screen/Community/CommunityPostFormScreen.tsx
+++ src/screen/Community/CommunityPostFormScreen.tsx
@@ -1,4 +1,6 @@
 import CTAButton from '@/components/Common/CTAButton';
+import useInput from '@/hooks/useInput';
+import TOAST from '@/utils/constants/toast';
 import {createCommunityPost} from '@api/community';
 import DismissKeyboardView from '@components/DismissKeyboardView';
 import Input, {KeyboardTypes} from '@components/Input';
@@ -8,7 +10,7 @@ import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {WHITE} from '@styles/colors';
 import common from '@styles/common';
 import {isAxiosError} from 'axios';
-import {useCallback, useState} from 'react';
+import {useCallback} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {LoggedInParamList} from '../../../AppInner';
 
@@ -18,19 +20,25 @@ const CHANNEL = ['필라테스', '요가', '릴리리맘보'];
 type Props = NativeStackScreenProps<LoggedInParamList, 'CommunityPostForm'>;
 
 const CommunityPostFormScreen = ({navigation}: Props) => {
-  // TODO: 로딩 상태 처리
-  const [title, setTitle] = useState('');
-  const [category, setCategory] = useState('');
-  const [contents, setContents] = useState('');
+  const titleInput = useInput();
+  const categoryInput = useInput();
+  const contentsInput = useInput();
 
-  const canGoNext = title && contents && category;
+  const isTitleValid = titleInput.value.length > 0;
+  const isCategoryValid = categoryInput.value.length > 0;
+  const isContentsValid = contentsInput.value.length > 0;
+  const isAllFieldValid = isTitleValid && isCategoryValid && isContentsValid;
 
   const onCreateCommunityPost = useCallback(() => {
-    const data = {category: category, title: title, contents: contents};
+    const data = {
+      category: categoryInput.value,
+      title: titleInput.value,
+      contents: contentsInput.value,
+    };
 
     createCommunityPost(data)
       .then(() => {
-        toast.success({message: '게시글 등록이 완료되었어요!'});
+        toast.success({message: TOAST.COMMUNITY_POST_SUCCESS});
         navigation.navigate('Community');
       })
       .catch(error => {
@@ -38,17 +46,17 @@ const CommunityPostFormScreen = ({navigation}: Props) => {
           toast.error({message: error.message});
         }
       });
-  }, [category, contents, navigation, title]);
+  }, [categoryInput.value, contentsInput.value, navigation, titleInput.value]);
 
   return (
     <DismissKeyboardView>
       <View style={styles.container}>
         <View style={common.mb16}>
           <Input
-            label={'제목'}
-            onChangeText={(text: string) => setTitle(text)}
-            value={title}
-            placeholder={'게시글 제목을 입력하세요.'}
+            label="제목"
+            onChangeText={titleInput.onChange}
+            value={titleInput.value}
+            placeholder="게시글 제목을 입력하세요."
             keyboardType={KeyboardTypes.DEFAULT}
             editable={true}
           />
@@ -56,19 +64,19 @@ const CommunityPostFormScreen = ({navigation}: Props) => {
 
         <View style={common.mb16}>
           <SelectBox
-            label={'채널'}
+            label="채널"
             data={CHANNEL}
-            onSelect={(value: any) => setCategory(value)}
-            defaultButtonText={'채널을 선택하세요.'}
+            onSelect={categoryInput.onChange}
+            defaultButtonText="채널을 선택하세요."
           />
         </View>
 
         <View style={common.mb16}>
           <Input
-            label={'게시글 내용'}
-            onChangeText={(text: string) => setContents(text)}
-            value={contents}
-            placeholder={'게시글 내용을 작성해 주세요.'}
+            label="게시글 내용"
+            onChangeText={contentsInput.onChange}
+            value={contentsInput.value}
+            placeholder="게시글 내용을 작성해 주세요."
             keyboardType={KeyboardTypes.DEFAULT}
             editable={true}
             multiline={true}
@@ -78,7 +86,7 @@ const CommunityPostFormScreen = ({navigation}: Props) => {
           <CTAButton
             label="게시글 등록"
             loading={LOADING}
-            disabled={!canGoNext}
+            disabled={!isAllFieldValid}
             onPress={onCreateCommunityPost}
           />
         </View>
--- src/utils/constants/toast.ts
+++ src/utils/constants/toast.ts
@@ -2,6 +2,7 @@ export const SHOW_TOAST_MESSAGE = 'SHOW_TOAST_MESSAGE';
 
 const TOAST = {
   LOGIN_SUCCESS: '로그인이 완료되었어요!',
+  COMMUNITY_POST_SUCCESS: '게시글 등록이 완료되었어요!',
 };
 
 export default TOAST;
