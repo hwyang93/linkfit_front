d10da752efd7eb6f9f20606024a4f3eb049e1003
nonon <nononcrust@gmail.com>
2023-09-17 19:13:10 +0900

    작성 후기 관리 스크린 UI 수정

--- src/screen/My/ReviewManageScreen.tsx
+++ src/screen/My/ReviewManageScreen.tsx
@@ -1,17 +1,164 @@
-import {materialTopTabNavigationOptions} from '@/utils/options/tab';
-import EmployeeReviewComponent from '@components/My/EmployeeReviewComponent';
-import EmployerReviewComponent from '@components/My/EmployerReviewComponent';
-import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
-
-const Tab = createMaterialTopTabNavigator();
+import BottomSheet from '@/components/Common/BottomSheet';
+import BottomSheetOption from '@/components/Common/BottomSheetOption';
+import {useDeleteMemberReputationMutation} from '@/hooks/member/useDeleteMemberReputationMutation';
+import {useMemberReputationListQuery} from '@/hooks/member/useMemberReputationListQuery';
+import toast from '@/hooks/toast';
+import {useAppNavigation} from '@/hooks/useAppNavigation';
+import useModal from '@/hooks/useModal';
+import {BLUE, GRAY, WHITE} from '@/styles/colors';
+import common from '@/styles/common';
+import {Member} from '@/types/common';
+import {iconPath} from '@/utils/iconPath';
+import {formatDate} from '@/utils/util';
+import {isAxiosError} from 'axios';
+import {useState} from 'react';
+import {
+  Image,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
 const ReviewManageScreen = () => {
+  const [selectedReputationId, setSelectedReputationId] = useState<
+    number | null
+  >(null);
+
+  const navigation = useAppNavigation();
+
+  const memberReputationListQuery = useMemberReputationListQuery();
+  const reputations = memberReputationListQuery.data;
+
+  const deleteMemberReputationMutation = useDeleteMemberReputationMutation();
+
+  const modal = useModal();
+
+  const deleteReputation = () => {
+    if (!selectedReputationId) return;
+
+    deleteMemberReputationMutation.mutate(selectedReputationId, {
+      onSuccess: () => {
+        toast.success({message: '후기 삭제가 완료되었어요!'});
+        modal.close();
+      },
+      onError: error => {
+        if (isAxiosError(error)) {
+          toast.error({message: error.message});
+        }
+      },
+    });
+  };
+
+  const onEditPress = () => {
+    modal.close();
+    navigation.navigate('ReviewForm', {
+      reputationInfo: selectedReputationId,
+    });
+  };
+
+  const onDeletePress = () => {
+    deleteReputation();
+  };
+
   return (
-    <Tab.Navigator screenOptions={materialTopTabNavigationOptions}>
-      <Tab.Screen name="구직" component={EmployeeReviewComponent} />
-      <Tab.Screen name="구인" component={EmployerReviewComponent} />
-    </Tab.Navigator>
+    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+      {reputations?.map(item =>
+        item.targetMember.type === Member.Instructor ? (
+          <View key={'review_' + item.seq} style={styles.reviewBox}>
+            <View style={common.rowCenter}>
+              <Image
+                source={
+                  item.targetMember.profileImage
+                    ? {uri: item.targetMember.profileImage.originFileUrl}
+                    : require('../../assets/images/thumbnail.png')
+                }
+                style={styles.thumbnail}
+              />
+              <View>
+                <View style={common.rowCenter}>
+                  <Text style={[common.text_m, common.fwb, common.mr8]}>
+                    {item.targetMember.nickname
+                      ? item.targetMember.nickname
+                      : item.targetMember.name}
+                  </Text>
+                  <Text style={[common.text_s, {color: BLUE.DEFAULT}]}>
+                    인증강사
+                  </Text>
+                  <Image
+                    style={{marginLeft: 2, width: 14, height: 14}}
+                    source={iconPath.CERTIFICATION}
+                  />
+                </View>
+                <View style={common.row}>
+                  <Text style={[common.text_s, common.fwb, common.mr8]}>
+                    {item.targetMember.field}
+                  </Text>
+                  {/*<Text style={[common.text]}>3년</Text>*/}
+                </View>
+              </View>
+            </View>
+            <Text style={[common.mt8, common.text]}>
+              {formatDate(item.updatedAt)}
+            </Text>
+            <Text style={common.text_m}>{item.comment}</Text>
+            <Pressable
+              style={styles.kebabIcon}
+              hitSlop={10}
+              onPress={() => {
+                setSelectedReputationId(item.seq);
+                modal.open();
+              }}>
+              <Image source={iconPath.KEBAB} style={[common.size24]} />
+            </Pressable>
+          </View>
+        ) : (
+          <View key={'review_' + item.seq} style={styles.reviewBox}>
+            <View style={common.rowCenter}>
+              <Text style={[common.text_m, common.fwb, common.mr8]}>
+                {item.targetMember.company?.companyName}
+              </Text>
+              <Text style={common.text}>
+                {item.targetMember.company?.field}
+              </Text>
+            </View>
+            <Text style={[common.mt8, common.text]}>
+              {formatDate(item.updatedAt)}
+            </Text>
+            <Text style={common.text_m}>{item.comment}</Text>
+            <Pressable
+              style={styles.kebabIcon}
+              hitSlop={10}
+              onPress={() => {
+                setSelectedReputationId(item.seq);
+                modal.open();
+              }}>
+              <Image source={iconPath.KEBAB} style={[common.size24]} />
+            </Pressable>
+          </View>
+        ),
+      )}
+      <BottomSheet
+        visible={modal.visible}
+        onDismiss={modal.close}
+        title="더보기">
+        <BottomSheetOption label="후기 수정하기" onPress={onEditPress} />
+        <BottomSheetOption label="후기 삭제하기" onPress={onDeletePress} />
+      </BottomSheet>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {flex: 1, padding: 16, backgroundColor: WHITE},
+  reviewBox: {
+    paddingVertical: 16,
+    borderBottomWidth: 1,
+    borderColor: GRAY.DEFAULT,
+  },
+  thumbnail: {marginRight: 12, width: 48, height: 48},
+  kebabIcon: {position: 'absolute', top: 16, right: 0},
+});
 
 export default ReviewManageScreen;
