59fe1ed919ede7ffe1bec6150f1e97c234b35f6e
nonon <nononcrust@gmail.com>
2023-10-02 17:52:23 +0900

    git 에러 수정

--- src/screen/Community/community-comment-edit.screen.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export const CommunityCommentEditScreen = () => {
-  return (
-    <View>
-      <Text>댓글 수정 스크린</Text>
-    </View>
-  );
-};
--- src/screen/My/resume-edit.screen.tsx
+++
@@ -1,5 +0,0 @@
-import { View } from 'react-native';
-
-export const ResumeEditScreen = () => {
-  return <View></View>;
-};
